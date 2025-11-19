db44e9f01fcd3cbc309a3e18dd99f70f035332a6
Karni Guez <karni.guez@post.runi.ac.il>
2025-11-19 16:46:41 +0200

    update 7

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,66 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		if (str1.length()!= str2.length()){
+			return false;
+		}
+		else{
+		int[] freq1 = new int [str1.length()];
+		int[] freq = new int [str2.length()];
+		for (int i = 0; i < str1.length(); i++ ){
+			for ( int j = 0; j < str2.length(); j++){
+				if (str1.charAt(i)== str2.charAt(j)){
+					freq[j]++;
+					freq1[i]++;
+				}
+			}
+		}
+		for (int k = 0; k < freq.length; k++){
+			if ( freq[k]==0 || freq1[k]==0){
+				return false;
+			
+			}else{
+				return true;
+			}
+		}
 	}
+	return true;
+}
+
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (Character.isLetter(c)){
+				c = Character.toLowerCase(c);
+			}
+			else if  (32 <= c && c <= 47 || 58 <= c && c <= 64 || 91 <= c && c <= 96 || 123 <= c && c <= 126){
+				str = str.substring(0 , i) + str.substring(i + 1);
+				i--;
+				continue;
+			}
+			str = str.substring(0 , i) + c + str.substring(i + 1);
+			}
+			return str;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		for (int i = 0; i < str.length(); i++){
+			int rand = (int) (Math.random() * str.length());
+			str += str.charAt(rand)	;
+			str = str.substring(0 , rand) + str.substring(rand + 1);
+
+			
+		}
+
+		return str;
 	}
 }
